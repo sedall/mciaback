aea85556e9625ad8fefefc2b22ac0269d8eb29e5
Khesali <mohammad.khesali79@gmail.com>
2026-07-19 15:52:48 +0330

    update project

--- Modules/Loans/routes/api.php
+++ Modules/Loans/routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Loans\Http\Controllers\LoanController;
+use Modules\Loans\Http\Controllers\LoansController;
 
 Route::middleware(['auth:sanctum', 'panel.access:customer'])
     ->prefix('customer')
     ->group(function () {
-        Route::get('loans', [LoanController::class, 'index']);
-        Route::post('loans', [LoanController::class, 'store']);
+        Route::get('loans', [LoansController::class, 'index']);
+        Route::post('loans', [LoansController::class, 'store']);
     });
